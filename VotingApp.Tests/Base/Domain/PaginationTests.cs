67769676fa57d9646572d8922fe8f550bc79f0c0
Jose Mata <mata649@hotmail.com>
2023-06-14 18:32:25 -0600

    feat: add pagination tests

---
+++ VotingApp.Tests/Base/Domain/PaginationTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VotingApp.Base.Domain;
+
+namespace VotingApp.Tests.Base.Domain
+{
+    public class PaginationTests
+    {
+        [Fact]
+        public void NewPagination_PropertiesEqualsOrLessThanZero_SetsDefaultValue()
+        {
+            var pagination = new Pagination()
+            {
+                CurrentPage = 0,
+                PageSize = 0
+            };
+
+            Assert.Equal(1, pagination.CurrentPage);
+            Assert.Equal(10, pagination.PageSize);
+        }
+    }
+}
